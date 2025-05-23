07f793c2f067bb8f66175dc3f4ae938ef6e136f8
tungdx05 <tungdxph50725@gmail.com>
2025-05-23 22:23:00 +0700

    Add forgot password feature with password reset form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import ClientLayout from "../components/layouts/ClientLayout";
 import HomePage from "../components/common/HomePage";
 import LoginForm from "../components/auth/LoginForm";
 import RegisterForm from "../components/auth/RegisterForm";
-
+import ForgotPasswordForm from '../components/auth/ForgotPasswordForm';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,6 +23,10 @@ const router = createBrowserRouter([
     path: "/register",
     element: <RegisterForm />,
   },
+  {
+  path: "/forgot-password",
+  element: <ForgotPasswordForm />,
+},
 ]);
 
 export default router;
